3a5e0f49410fb13783479dce53263b7b7decf6b8
Glitch (rest-timestamp-fcc) <none>
2018-08-22 04:00:48 +0000

    💇👔 Checkpoint ./server.js:1639572/350

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/timestamp/:date_string", function (req, res) {
+  
+  console.log(req.params.date_string)
   res.json({greeting: 'hello API'});
 });
 
